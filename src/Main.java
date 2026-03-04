4ee17a00e090116f8a599282f57e5eff4be69ff5
Celeste <celeste.ag.2c@gmail.com>
2026-03-04 11:51:10 +0100

    conflictos merge resueltos

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner entrada = new Scanner(System.in);
+
         GestorTareas gestor = new GestorTareas();
+
         int opcion = 0;
 
-        System.out.println("\n--- MENÚ DE TAREAS ---");
-        System.out.println("3. Salir");
-        System.out.print("Elige una opción: ");
+        while (opcion != 4) {
 
-        opcion = entrada.nextInt();
+            System.out.println("\n--- MENÚ DE TAREAS ---");
 
-        switch (opcion) {
-            case 1:
-                System.out.print("Escribe la tarea: ");
-                String texto = entrada.nextLine();
-                gestor.añadirTarea(texto);
-                System.out.println("Tarea guardada.");
-                break;
-            case 2:
-                System.out.println("Saliendo...");
-                break;
-            case 3:
-                System.out.println("Saliendo...");
-                break;
+            System.out.println("1. Nueva Tarea");
+
+            System.out.println("2. Listar Tareas");
+
+            System.out.println("4. Salir");
+
+            System.out.print("Elige una opción: ");
+
+            opcion = entrada.nextInt();
+
+            entrada.nextLine();
+
+            switch (opcion) {
+
+                case 1:
+
+                    System.out.print("Escribe la tarea: ");
+
+                    gestor.añadirTarea(entrada.nextLine());
+
+                    break;
+
+                case 2:
+
+                    gestor.mostrarTareas();
+
+                    break;
+
+            }
+
         }
+
     }
+
 }
