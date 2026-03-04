2bf91ec788cc2b0f6c65fd4ada47c81ec3c0bc7a
vinssoke <marcoo1771@gmail.com>
2026-03-04 10:14:16 +0000

    Estructura inicial: Clase Tarea

---
+++ src/Tarea.java
@@ -0,0 +1,30 @@
+
+public class Tarea {
+
+    private String descripcion;
+
+    private boolean completada;
+
+    public Tarea(String descripcion) {
+
+        this.descripcion = descripcion;
+
+        this.completada = false;
+
+    }
+
+    public String getDescripcion() { return descripcion; }
+
+    public boolean isCompletada() { return completada; }
+
+    public void setCompletada(boolean completada) { this.completada = completada; }
+
+    @Override
+
+    public String toString() {
+
+        return "[" + (completada ? "X" : " ") + "] " + descripcion;
+
+    }
+
+}
